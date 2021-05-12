3809132c18c6f597899195ee07339c25231c51da
behosseini <b.hosseini@hotmail.com>
2021-05-12 20:30:03 +0100

    Changed build directory to lib

--- example/index.tsx
+++ example/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "../src";
-// import App from "../dist";
+// import App from "../lib";
 
 const root = document.createElement("DIV");
 root.setAttribute("id", "root");
